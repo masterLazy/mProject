dda8b9b978eb68ad03f98683357fb04b7536ecfe
mLazy <95967139+masterLazy@users.noreply.github.com>
2023-02-19 09:35:45 +0800

    Create c_sharp.cs

---
+++ Hello_world_100/c_sharp.cs
@@ -0,0 +1,20 @@
+
+using System;
+namespace HelloWorldApplication
+{
+  class HelloWorld
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+      Console.WriteLine("Hello, world!");
+    }
+}
