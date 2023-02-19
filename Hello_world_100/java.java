cdd71fe906fa7fd8f75b272794af3260f5ccd89c
mLazy <95967139+masterLazy@users.noreply.github.com>
2023-02-19 09:32:10 +0800

    Update java.java

--- Hello_world_100/java.java
+++ Hello_world_100/java.java
@@ -2,15 +2,15 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
+    System.out.print("Hello, world!");
   }
 }
